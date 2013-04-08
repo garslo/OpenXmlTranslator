758ed08bcd2edbcabd2ba7c0cd4682b9dd5e5489
Gary Slopsema <gslopsema@gmail.com>
2013-04-07 21:23:19 -0400

    Added some concrete implementations
    Created implementations of the GenericFactory and Translator interfaces.

---
+++ OpenXmlTranslator/LaTeX/LaTeXFactory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Math;
+
+namespace OpenXmlTranslate.LaTeX
+{
+    public class LaTeXFactory : GenericFactory<OpenXmlElement, LaTeXElement>
+    {
+        private ElementConverter<OpenXmlElement, LaTeXElement> converter;
+
+        public LaTeXFactory(ElementConverter<OpenXmlElement, LaTeXElement> theConverter)
+        {
+            converter = theConverter;
+        }
+
+        public LaTeXElement MakeFrom(OpenXmlElement element)
+        {
+            return converter.Convert(element);
+        }
+    }
+}
---
+++ OpenXmlTranslator/LaTeX/OpenXmlToLaTeXTranslator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using DocumentFormat.OpenXml;
+
+namespace OpenXmlTranslate.LaTeX
+{
+    public class OpenXmlToLaTeXTranslator : Translator<OpenXmlElement, LaTeXElement>
+    {
+        private LaTeXFactory factory;
+        public OpenXmlToLaTeXTranslator(LaTeXFactory latexFactory)
+        {
+            factory = latexFactory;
+        }
+
+        public LaTeXElement Translate(OpenXmlElement element)
+        {
+            return factory.MakeFrom(element);
+        }
+    }
+}
