1f934c845d8ff0049b1acb3cd5d68c79d850b189
Gary Slopsema <gslopsema@gmail.com>
2013-04-07 21:21:24 -0400

    Added abstract interfaces
    This is the basic pattern everything will follow:
    
    A Translator<> translates OpenXmlElements to LaTeXElements using a
    GenericFactory<>, which uses an ElementConverter<> to create the proper
    class.

---
+++ OpenXmlTranslator/Translator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenXmlTranslate
+{
+    public interface Translator<FromType, ToType>
+    {
+        ToType Translate(FromType toTranslate);
+    }
+}
